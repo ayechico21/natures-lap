4bd7f597cc5d85f1bde67ca1027216b62e6e8aea
chico <ayechico21@gmail.com>
2024-09-01 16:57:28 +0530

    test deploy

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   build: {
-    assetsDir: ".",
+    assetsDir: "assets",
   },
   define: { "process.env": {} },
 });
